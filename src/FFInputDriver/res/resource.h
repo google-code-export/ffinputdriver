1395ead3f2b73d690ce7afa8437b8c7c44d2c89e
aka.xzib1t@gmail.com <aka.xzib1t@gmail.com@852485ad-f269-359c-2837-91ef1518b067>
2011-08-03 12:17:32 +0000

    -added format *.mxf;*.ogm;*.qt;*.tp;*.dvr-ms;*.amv to open pattern; -fixed PAR adjust; -updated ffmpeg to 0.8.1 -code cleaning

--- src/FFInputDriver/res/resource.h
+++ src/FFInputDriver/res/resource.h
@@ -4,15 +4,9 @@
 //
 #define IDOK                            1
 #define IDCANCEL                        2
-#define IDD_DIALOG1                     101
-#define IDD_FFMPEG_OPTIONS              101
 #define IDD_FF_OPTIONS                  101
 #define IDD_FF_INFO                     103
-#define IDC_EDIT1                       1001
-#define IDC_CHECK1                      1002
-#define IDC_VIDEO_RESIZEPAR             1002
 #define IDC_VIDEO_ADJUSTPAR             1002
-#define IDC_SCROLLBAR1                  1003
 #define IDC_AUDIO_DOWNMIX               1003
 #define IDC_FORMATNAME                  1004
 #define IDC_VIDEOTEXT1                  1007
@@ -21,35 +15,16 @@
 #define IDC_VIDEO_BITRATE               1010
 #define IDC_VIDEO_FRAMERATE             1011
 #define IDC_VIDEO_ASPECTRATIO           1013
-#define IDC_VIDEO_FPS                   1054
 #define IDC_VIDEO_CODECNAME             1054
-#define IDC_AUDIO_SAMPLINGRATE          1056
 #define IDC_AUDIO_CODECNAME             1056
 #define IDC_AUDIO_CHANNELS              1057
-#define IDC_AUDIO_PRECISION             1058
 #define IDC_AUDIO_SAMPLEFMT             1058
-#define IDC_AUDIO_NUMFRAMES             1059
-#define IDC_AUDIO_SAMPLINGRATE2         1059
-#define IDC_AUDIO_FRAMESIZES            1064
-#define IDC_AUDIO_PRELOAD               1065
 #define IDC_AUDIO_LAYOUT                1065
-#define IDC_AUDIO_COMPRESSION           1066
 #define IDC_AUDIO_SAMPLERATE            1066
-#define IDC_AUDIO_CHANNELS2             1067
 #define IDC_DURATION                    1067
-#define IDC_AUDIO_PRECISION2            1068
 #define IDC_STARTTIME                   1068
-#define IDC_AUDIO_LAYOUT2               1069
 #define IDC_STREAMSCOUNT                1069
-#define IDC_AUDIO_COMPRESSION2          1071
 #define IDC_BITRATE                     1071
-#define IDC_VIDEO_SIZE                  1216
-#define IDC_VIDEO_RATE                  1217
-#define IDC_VIDEO_RATIO                 1219
-#define IDC_AUDIO_RATE                  1220
-#define IDC_AUDIO_DATARATE              1221
-#define IDC_AUDIO_RATIO                 1222
-#define IDC_AUDIO_LENGTH                1404
 #define IDC_AUDIO_BITRATE               1404
 
 // Next default values for new objects
